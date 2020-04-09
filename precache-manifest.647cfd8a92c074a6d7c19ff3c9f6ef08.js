c649f9e462403f7b07bf93711bfba03ae473038e
Kevin Hao <k.hao@wustl.edu>
2020-04-09 18:11:41 -0500

    Updates

---
+++ precache-manifest.647cfd8a92c074a6d7c19ff3c9f6ef08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a04d1524f5c465226600",
+    "url": "/09-todo-react-kevinxhao/static/css/main.1af24ae9.chunk.css"
+  },
+  {
+    "revision": "a04d1524f5c465226600",
+    "url": "/09-todo-react-kevinxhao/static/js/main.a04d1524.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/09-todo-react-kevinxhao/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "c50805241324bcd956da",
+    "url": "/09-todo-react-kevinxhao/static/js/runtime~main.c5080524.js"
+  },
+  {
+    "revision": "506d2f2e96be54dec9a8f4ff470d6a03",
+    "url": "/09-todo-react-kevinxhao/index.html"
+  }
+];
